a88a0d7b8c032328fcfc3a21e9007d683fb85c65
megha083 <79038938+megha083@users.noreply.github.com>
2023-03-22 19:20:34 +0530

    solved

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { Meta, Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -8,13 +7,5 @@ import { Meta, Title } from '@angular/platform-browser';
 })
 
 export class AppComponent {
-  //title = 'syu';
-
-  constructor(
-    private title: Title,
-    private meta: Meta,){}
-
-    ngOnInit() {
-  this.meta.addTag({ name: 'Content-Security-Policy', content: "default-src 'self' https://www.selectyouruniversity.com/; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.selectyouruniversity.com/;" });
-}
+  title = 'syu';
 }
